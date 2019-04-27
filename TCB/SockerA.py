9e63b86add2e2fbdf7bf6ed75d57db316b342172
Srirath Sawetsuttipun <srirath.sawetsuttipun@dga.or.th>
2019-04-27 19:40:46 +0700

    commit

--- TCB/SockerA.py
+++ TCB/SockerA.py
@@ -1,57 +1,138 @@
-import asyncore
 import socket 
+import asyncore 
+import os
+import base64
+import dlib
+import cv2
+import openface
+import numpy as np 
+import gc
 
+from io import BytesIO
+from PIL import Image
+
+np.set_printoptions(precision=2)
+
+from interface import implements, Interface 
+
+class AlignmentInterface(Interface): 
+    def execute(self, base64Img,align):
+        pass 
+
+class VerifyInterface(Interface): 
+    def execute(self, known_img,unknown_img,net):
+        pass 
+
+class AdapterAlignment(implements(AlignmentInterface)):
+    def execute(self, base64Img,align): 
+      
+        sbuf = BytesIO()
+        sbuf.write(base64.b64decode(base64Img))
+        pimg = Image.open(sbuf) 
+        rgbImg = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)   
+
+        bb = align.getLargestFaceBoundingBox(rgbImg)  
+        alignedFace = align.align(96, rgbImg, bb,landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+        del sbuf
+        del pimg
+        del rgbImg
+        del bb 
+        return alignedFace
+
+class AdapterVerification(implements(VerifyInterface)): 
+    def execute(self, known_img,unknown_img,net): 
+       
+        try:
+            img1 = net.forward(known_img)
+            img2 = net.forward(unknown_img) 
+            d = img1 - img2
+            del net
+            del img1
+            del img2 
+            return "{:0.3f}".format(np.dot(d, d))
+        except:
+            return None
+        
+        
+
+  
+
+class EchoHandler(asyncore.dispatcher_with_send): 
  
-class Server(asyncore.dispatcher):
-    def __init__(self, address):
-        asyncore.dispatcher.__init__(self) 
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind(address)
-        self.address = self.socket.getsockname()
-        self.listen(5)
+   
+    def handle_read(self):  
+        data = self.recv(5000) 
+        if data:  
+            data = data.decode('utf-8')
+            
+            if(data[-2:] =="##"):
 
-    def handle_accept(self):
-        # Called when a client connects to our socket
-        client_info = self.accept()
-        if client_info is not None:
-            ClientHandler(client_info[0], client_info[1])
-    
+                read = open("base_1.txt", "r")
+                encodeImage = read.read() +data
+                read.close() 
 
-class ClientHandler(asyncore.dispatcher):
-    def __init__(self, sock, address):
-        asyncore.dispatcher.__init__(self, sock)
-        self.data_to_write = []
+                alignment = AdapterAlignment() 
+                align = openface.AlignDlib('shape_predictor/shape_predictor_68_face_landmarks.dat')
+                
+                fileDir = os.path.dirname(os.path.realpath(__file__))
+                modelDir = os.path.join(fileDir, '..', 'models')
+                openfaceModelDir = os.path.join(modelDir, 'openface')
+ 
+             
+                source =encodeImage.split('|')
+                encode_1 = source[0]
+                encode_2 = source[1]  
+         
 
-    def writable(self):
-        return bool(self.data_to_write)
+                result_1= alignment.execute(encode_1,align) 
+                result_2= alignment.execute(encode_2[:-2],align) 
 
-    def handle_write(self):
-        data = self.data_to_write.pop()
-        sent = self.send(data[:1024]) 
-        print('handle_write')
-        if sent < len(data): 
-            print('sent < len(data)')
-            remaining = data[sent:]
-            self.data_to_write.append(remaining) 
+                verify = AdapterVerification()
+                net = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), 96) 
+                result = verify.execute(result_1,result_2,net)
 
-    def handle_read(self):
-        data = self.recv(1024)
-        if(len(data) > 0):  
-            print('handle_read() -> (%d) "%s"', len(self.data_to_write), data.rstrip()) 
-            self.data_to_write.insert(0, data)
-    
-    def handle_close(self): 
+                if(result is not None):
+                    if(float(result) < 1.00):
+                        result = 'true|'+result
+                    else:
+                        result = 'false|'+result 
+                else:
+                    result = 'None' 
+                print("RESULT : "+result) 
+                gc.collect()
+     
+            else: 
+                f = open("base_1.txt", "a")
+                f.write(data)
+                f.close()  
+                result ='xxx'
+  
+            self.send(result.encode())  
+     
+    def handle_close(self):
+        print('handle_close') 
+        os.remove("base_1.txt")
         self.close()
 
+    
 
 
-def main(): 
-    HOST = '0.0.0.0'
-    PORT = 4000
-    s = Server((HOST,PORT))
-    asyncore.loop()
+class EchoServer(asyncore.dispatcher):
 
+    def __init__(self, host, port): 
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(1)   
 
-if __name__ == '__main__':
-    main() 
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair 
+            print 'Incoming connection from %s' % repr(addr)
+            handler = EchoHandler(sock)
+          
+
+server = EchoServer('', 4000)
+asyncore.loop()
